e041bfbc4c9030e7dd861e61a6ea1992e906345b
Miroslav Harag <m.harag.v@gmail.com>
2024-05-05 22:55:56 +0200

    finish README

--- experiments/train.py
+++ experiments/train.py
@@ -3,7 +3,7 @@ from fen.dataset import CGPDataset
 from fen.trainer import Trainer
 
 #os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
-num_epochs = 20
+num_epochs = 40
 
 def main():
     train_dataset = CGPDataset("datasets/ariths_gen_100_high_prec_augmented/train")
@@ -12,10 +12,10 @@ def main():
 
     print('[INFO] Create Model and Trainer')
     model = FEN()
-    model.load("./exp/augmented1/model_15.pth")
+    model.load("./pretrained/ariths_gen1.pth")
     print(f'[INFO] Model created parameters: {model.n_param()}')
 
-    trainer = Trainer(model, training_id="augmented2", lr=5e-3)
+    trainer = Trainer(model, training_id="small", lr=5e-3)
 
     print('[INFO] Training ...')
     trainer.train(num_epochs, train_dataset, val_dataset)
